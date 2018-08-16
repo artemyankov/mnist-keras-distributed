6fe15859fe9c7b741c1a4353e0e98bceaa8843b6
Artem Yankov <yankovai3@gmail.com>
2018-08-16 08:54:43 -0700

    Added distributed example

---
+++ mnist_distributed.py
@@ -0,0 +1,195 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from keras.metrics import categorical_accuracy as accuracy
+import keras
+
+
+#
+# Data
+#
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# ----- Insert that snippet to run distributed jobs -----
+
+from clusterone import get_data_path, get_logs_path
+
+# Configure  distributed task
+try:
+  job_name = os.environ['JOB_NAME']
+  task_index = os.environ['TASK_INDEX']
+  ps_hosts = os.environ['PS_HOSTS']
+  worker_hosts = os.environ['WORKER_HOSTS']
+except:
+  job_name = None
+  task_index = 0
+  ps_hosts = None
+  worker_hosts = None
+
+flags = tf.app.flags
+
+# Flags for configuring the distributed task
+flags.DEFINE_string("job_name", job_name,
+                    "job name: worker or ps")
+flags.DEFINE_integer("task_index", task_index,
+                     "Worker task index, should be >= 0. task_index=0 is "
+                     "the chief worker task that performs the variable "
+                     "initialization and checkpoint handling")
+flags.DEFINE_string("ps_hosts", ps_hosts,
+                    "Comma-separated list of hostname:port pairs")
+flags.DEFINE_string("worker_hosts", worker_hosts,
+                    "Comma-separated list of hostname:port pairs")
+
+# Training related flags
+# flags.DEFINE_string("data_dir",
+#                     get_data_path(
+#                         dataset_name = "malo/mnist", #all mounted repo
+#                         local_root = ROOT_PATH_TO_LOCAL_DATA,
+#                         local_repo = "mnist",
+#                         path = 'data'
+#                         ),
+#                     "Path to store logs and checkpoints. It is recommended"
+#                     "to use get_logs_path() to define your logs directory."
+#                     "so that you can switch from local to clusterone without"
+#                     "changing your code."
+#                     "If you set your logs directory manually make sure"
+#                     "to use /logs/ when running on ClusterOne cloud.")
+flags.DEFINE_string("log_dir",
+                    get_logs_path('/Users/artem/Documents/Scratch/mnist_keras_distributed/logs/'),
+                    "Path to dataset. It is recommended to use get_data_path()"
+                    "to define your data directory.so that you can switch "
+                    "from local to clusterone without changing your code."
+                    "If you set the data directory manually makue sure to use"
+                    "/data/ as root path when running on ClusterOne cloud.")
+FLAGS = flags.FLAGS
+
+def device_and_target():
+    # If FLAGS.job_name is not set, we're running single-machine TensorFlow.
+    # Don't set a device.
+    if FLAGS.job_name is None:
+        print("Running single-machine training")
+        return (None, "")
+
+    # Otherwise we're running distributed TensorFlow
+    print("Running distributed training")
+    if FLAGS.task_index is None or FLAGS.task_index == "":
+        raise ValueError("Must specify an explicit `task_index`")
+    if FLAGS.ps_hosts is None or FLAGS.ps_hosts == "":
+        raise ValueError("Must specify an explicit `ps_hosts`")
+    if FLAGS.worker_hosts is None or FLAGS.worker_hosts == "":
+        raise ValueError("Must specify an explicit `worker_hosts`")
+
+    cluster_spec = tf.train.ClusterSpec({
+        "ps": FLAGS.ps_hosts.split(","),
+        "worker": FLAGS.worker_hosts.split(","),
+    })
+    server = tf.train.Server(
+        cluster_spec, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
+    if FLAGS.job_name == "ps":
+        server.join()
+
+    worker_device = "/job:worker/task:{}".format(FLAGS.task_index)
+    # The device setter will automatically place Variables ops on separate
+    # parameter servers (ps). The non-Variable ops will be placed on the workers.
+    return (
+        tf.train.replica_device_setter(
+        worker_device=worker_device,
+        cluster=cluster_spec),
+        server.target,
+)
+
+# --- end of snippet ----
+
+
+def main(_):
+    tf.reset_default_graph()
+    device, target = device_and_target()  # getting node environment
+
+    with tf.device(device):
+        global_step = tf.train.get_or_create_global_step()
+
+        # set Keras learning phase to train
+        keras.backend.set_learning_phase(1)
+        # do not initialize variables on the fly
+        keras.backend.manual_variable_initialization(True)
+
+        # Build Keras model
+        model_inp = keras.layers.Input(shape=(784,))
+        x = keras.layers.Dense(128, activation='relu')(model_inp)
+        x = keras.layers.Dense(128, activation='relu')(x)
+        model_out = keras.layers.Dense(10, activation='softmax')(x)
+        model = keras.models.Model(model_inp, model_out)
+
+        # keras model predictions
+        predictions = model.output
+        # placeholder for training targets
+        targets = tf.placeholder(tf.float32, shape=(None, 10))
+        # our categorical crossentropy loss
+        loss = tf.reduce_mean(
+            keras.losses.categorical_crossentropy(targets, predictions)
+        )
+        # accuracy
+        acc_value = tf.reduce_mean(accuracy(targets, predictions))
+
+        # Only if you have regularizers, not in this example
+        total_loss = loss * 1.0  # Copy
+        for regularizer_loss in model.losses:
+            tf.assign_add(total_loss, regularizer_loss)
+
+        optimizer = tf.train.AdamOptimizer()
+
+        # Barrier to compute gradients after updating moving avg of batch norm
+        with tf.control_dependencies(model.updates):
+            barrier = tf.no_op(name="update_barrier")
+
+        with tf.control_dependencies([barrier]):
+            grads = optimizer.compute_gradients(
+                total_loss,
+                model.trainable_weights
+            )
+            grad_updates = optimizer.apply_gradients(grads, global_step=global_step)
+
+        with tf.control_dependencies([grad_updates]):
+            train_op = tf.identity(total_loss, name="train")
+
+
+
+
+    #
+    # Train
+    #
+    with tf.train.MonitoredTrainingSession(master=target, is_chief=(FLAGS.task_index == 0), checkpoint_dir=FLAGS.log_dir) as sess:
+        # writer.add_graph(sess.graph)
+        for i in range(2000):
+            batch_x, batch_y = mnist.train.next_batch(128)
+
+            # perform the operations we defined earlier on batch
+            loss_value = sess.run(
+                [train_op],
+                feed_dict = {
+                    model.inputs[0]: batch_x,
+                    targets: batch_y
+                }
+            )
+
+            val_acc = sess.run(
+                acc_value,
+                feed_dict={
+                    model.inputs[0]: mnist.test.images,
+                    targets: mnist.test.labels
+                }
+            )
+
+            # val_loss = sess.run(
+            #     total_loss,
+            #     feed_dict={
+            #         model.inputs[0]: mnist.test.images,
+            #         targets: mnist.test.labels
+            #     }
+            # )
+
+            print ('{0}: Accuracy: {1}'.format(i, val_acc))
+
+
+if __name__ == '__main__':
+    tf.app.run()
